75982885c6784731853e1e35c9b9b90e91f83ab3
Atsushi Morimoto <74th.tech@gmail.com>
2026-02-23 19:57:44 +0900

    support M5Atom Echo S3R

--- examples/HiStackChanWakeUpWord/HiStackChanWakeUpWord.ino
+++ examples/HiStackChanWakeUpWord/HiStackChanWakeUpWord.ino
@@ -4,42 +4,59 @@
 #define USE_STEREO 0
 #define AUDIO_SAMPLE_SIZE 256
 
-void onSrEvent(sr_event_t event, int command_id, int phrase_id)
-{
+static bool display_available;
+
+void onSrEvent(sr_event_t event, int command_id, int phrase_id) {
   (void)command_id;
   (void)phrase_id;
 
-  if (event == SR_EVENT_WAKEWORD)
-  {
-    M5.Display.fillScreen(TFT_GREEN);
-    M5.Display.setCursor(10, 10);
-    M5.Display.setTextSize(3);
-    M5.Display.setTextColor(TFT_BLACK, TFT_GREEN);
-    M5.Display.println("WakeWord!");
-    M5.Display.println("Detected!");
+  if (event == SR_EVENT_WAKEWORD) {
+    Serial.println("WakeWord Detected!");
+    if (display_available) {
+      M5.Display.fillScreen(TFT_GREEN);
+      M5.Display.setCursor(10, 10);
+      M5.Display.setTextSize(3);
+      M5.Display.setTextColor(TFT_BLACK, TFT_GREEN);
+      M5.Display.println("WakeWord!");
+      M5.Display.println("Detected!");
+    }
 
     delay(1200);
 
-    M5.Display.fillScreen(TFT_BLACK);
-    M5.Display.setCursor(0, 0);
-    M5.Display.setTextSize(2);
-    M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Display.println("Listening...");
-    ESP_SR_M5.setMode(SR_MODE_WAKEWORD);
+    Serial.println("Listening...");
+    if (display_available) {
+      M5.Display.fillScreen(TFT_BLACK);
+      M5.Display.setCursor(0, 0);
+      M5.Display.setTextSize(2);
+      M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
+      M5.Display.println("Listening...");
+      ESP_SR_M5.setMode(SR_MODE_WAKEWORD);
+    }
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   delay(100);
 
   auto cfg = M5.config();
+
+  // Atom Echo S3R の場合
+  // cfg.internal_mic = true;
+
+  // Atom S3R と Atomic Echo Base の場合
+  // cfg.external_speaker.atomic_echo = 1;
+
   M5.begin(cfg);
 
-  M5.Display.setRotation(1);
-  M5.Display.setTextSize(2);
-  M5.Display.println("Init...");
+  display_available = M5.getDisplayCount() > 0;
+
+  Serial.println("Init...");
+  if (display_available) {
+    M5.Display.setRotation(1);
+    M5.Display.setTextSize(2);
+    M5.Display.println("Init...");
+  }
 
   auto mic_cfg = M5.Mic.config();
   mic_cfg.sample_rate = 16000;
@@ -50,44 +67,49 @@ void setup()
   ESP_SR_M5.onEvent(onSrEvent);
 
   bool success = ESP_SR_M5.begin(
-      nullptr,
-      0,
-      SR_MODE_WAKEWORD,
-      USE_STEREO ? SR_CHANNELS_STEREO : SR_CHANNELS_MONO);
+    nullptr,
+    0,
+    SR_MODE_WAKEWORD,
+    USE_STEREO ? SR_CHANNELS_STEREO : SR_CHANNELS_MONO);
 
-  M5.Display.fillScreen(TFT_BLACK);
-  M5.Display.setCursor(0, 0);
 
-  if (success)
-  {
-    M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
-    M5.Display.println("Listening...");
-    M5.Display.setTextSize(1);
-    M5.Display.println("Say: 'Hi Stack Chan'");
-  }
-  else
-  {
-    M5.Display.setTextColor(TFT_RED, TFT_BLACK);
-    M5.Display.println("Init Failed!");
+  if (success) {
+    Serial.println("Listening...");
+    Serial.println("Say: 'Hi Stack Chan'");
+
+    if (display_available) {
+      M5.Display.fillScreen(TFT_BLACK);
+      M5.Display.setCursor(0, 0);
+      M5.Display.setTextColor(TFT_WHITE, TFT_BLACK);
+      M5.Display.println("Listening...");
+      M5.Display.setTextSize(1);
+      M5.Display.println("Say: 'Hi Stack Chan'");
+    }
+  } else {
+    Serial.println("Init Failed!");
+
+    if (display_available) {
+      M5.Display.fillScreen(TFT_BLACK);
+      M5.Display.setCursor(0, 0);
+      M5.Display.setTextColor(TFT_RED, TFT_BLACK);
+      M5.Display.println("Init Failed!");
+    }
   }
 }
 
-void loop()
-{
+void loop() {
   M5.update();
 
 #if USE_STEREO
   static int16_t audio_buf[AUDIO_SAMPLE_SIZE * 2];
   bool ok = M5.Mic.record(audio_buf, AUDIO_SAMPLE_SIZE, 16000, true);
-  if (ok)
-  {
+  if (ok) {
     ESP_SR_M5.feedAudio(audio_buf, AUDIO_SAMPLE_SIZE * 2);
   }
 #else
   static int16_t audio_buf[AUDIO_SAMPLE_SIZE];
   bool ok = M5.Mic.record(audio_buf, AUDIO_SAMPLE_SIZE, 16000, false);
-  if (ok)
-  {
+  if (ok) {
     ESP_SR_M5.feedAudio(audio_buf, AUDIO_SAMPLE_SIZE);
   }
 #endif
